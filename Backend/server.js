8be2fbec011584f674070d5ccde9255e863a79d3
RahulBhatt24 <rbhatt06@gmail.com>
2022-10-19 21:30:25 -0500

    started http tutorial

--- Backend/server.js
+++ Backend/server.js
@@ -1,1 +1,12 @@
-console.log("hello world")
+const http = require('http');
+
+const server = http.createServer(function(req, res){
+    console.log(req.method);
+    res.end();
+});
+
+const PORT = 5000;
+
+server.listen(PORT, function() {
+    console.log(`Server running on port ${PORT}`)
+});
