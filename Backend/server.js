a788b09084657cd94e765b8c5b1be1ed7bc33f10
Diwakar Basak <diwakarbasak@gmail.com>
2022-10-31 10:29:44 -0500

    updated backend/server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,1 +1,34 @@
-console.log("hello world")
+
+const http = require('http');
+
+const todos = [
+    {id: 1, text: 'Todo One'},
+    {id: 2, text: 'Todo Two'},
+    {id: 3, text: 'Todo Three'}
+]
+
+const server = http.createServer(function(req, res){
+    res.writeHead(200, {
+        'Content-Type': 'application/json',
+        'X-Powered-By': 'Node.js'
+    });
+    let body = [];
+
+    req.on('data', chunk => {
+        body.push(chunk);
+    }).on('end', () => {
+        body = Buffer.concat(body).toString();
+        console.log(body);
+    })
+
+    res.end(JSON.stringify({
+        success: true,
+        data: todos
+    }));
+});
+
+const PORT = 5000;
+
+server.listen(PORT, function() {
+    console.log(`Server running on port ${PORT}`)
+});
