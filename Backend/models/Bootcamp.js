a29c293097139d0ccdf21378c7006a91b6fd78b7
RahulBhatt24 <rbhatt06@gmail.com>
2022-11-27 20:29:10 -0500

    made create bootcamp route

--- Backend/models/Bootcamp.js
+++ Backend/models/Bootcamp.js
@@ -34,24 +34,24 @@ const BootcampSchema = new mongoose.Schema({
         type: String,
         required: [true, 'Please add an address']
     },
-    location: {
-        type: {
-            type: String,
-            enum: ['Point'],
-            required: true
-        },
-        coordinates: {
-            type: [Number],
-            required: true,
-            index: '2dsphere'
-        },
-        formattedAddress: String,
-        street: String,
-        city: String,
-        state: String,
-        zipcode: String,
-        country: String
-    },
+    // location: {
+    //     type: {
+    //         type: String,
+    //         enum: ['Point'],
+    //         required: true
+    //     },
+    //     coordinates: {
+    //         type: [Number],
+    //         required: true,
+    //         index: '2dsphere'
+    //     },
+    //     formattedAddress: String,
+    //     street: String,
+    //     city: String,
+    //     state: String,
+    //     zipcode: String,
+    //     country: String
+    // },
     careers: {
         // Array of Strings
         type: [String],
